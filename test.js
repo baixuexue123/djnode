9504868382f5fa89db0a6ae4fd4c414fbfde1f1c
baixue <pysaoke@gmail.com>
2016-10-30 18:22:06 +0800

    add ws dep

---
+++ test.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var http = require('http');
+var server = http.createServer();
+var io = require('socket.io')(server);
+
+
+io.on('connection', function (socket) {
+  socket.on('event', function(data){
+      console.log(data);
+  });
+  socket.on('disconnect', function(){
+      console.log('disconnect');
+  });
+});
+
+server.listen(3000);
