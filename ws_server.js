e42dcc085b33f840641f0347fab9614ee433d015
baixue <pysaoke@gmail.com>
2016-10-31 22:47:50 +0800

    ws

--- ws_server.js
+++ ws_server.js
@@ -6,17 +6,18 @@ var wss = new WebSocketServer({ port: 8888 });
 
 wss.on('connection', function (ws) {
 
-    ws.on('open', function () {
-        console.log('open');
-    });
+    console.log(typeof ws);
 
     ws.on('message', function (message) {
+        var msg = JSON.$.parse(message);
         console.log('received: %s', message);
     });
 
     ws.send('something');
 
     ws.on('close', function (code, message) {
+        console.log(code);
+        console.log(message);
         console.log('close');
     });
 });
